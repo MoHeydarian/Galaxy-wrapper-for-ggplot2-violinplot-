675f745edd94b2ebfca57cabe36b06f5e8db6203
MoHeydarian <mheydarian@gmail.com>
2017-02-24 16:22:17 -0500

    updated options
    Brought over customization options from ggplot scatterplot wrapper

--- ggplotscriptviolin.R
+++ ggplotscriptviolin.R
@@ -30,6 +30,28 @@ option_specification = matrix(c(
   'yaxismin', 'g', 2, 'integer',
   'yaxismax', 'h', 2, 'integer',
   'scaling', 'b', 2, 'character',
+  'theme', 'l', 2, 'character',
+  'dim', 'k', 2, 'character',
+  'woutputdim', 'c', 2, 'integer',
+  'houtputdim', 'd', 2, 'integer',
+  'factor', 'n', 2, 'character',
+  'factorcol', 'm', 2, 'integer',
+  'colors', 'q', 2, 'character', 
+  'colororder', 'r', 2, 'integer', 
+  'pointcolor', 'u', 2, 'character', 
+  'axistitlecust', 'v', 2, 'character',
+  'axistitlecolor', '1', 2, 'character',
+  'axistitlesize', '2', 2, 'integer',
+  'axistitleface', '3', 2, 'character',
+  'axistextcust', '4', 2, 'character',
+  'axistextcolor', '5', 2, 'character',
+  'axistextsize', '6', 2, 'integer',
+  'axistextface', '7', 2, 'character',
+  'plottitlecust', '8', 2, 'character',
+  'plottitlecolor', '9', 2, 'character',
+  'plottitlesize', '10', 2, 'integer',
+  'plottitleface', '11', 2, 'character',
+  'gridlinecust', '12', 2, 'character',
   'output', 'o', 2, 'character'
   ), byrow=TRUE, ncol=4);
 
@@ -46,6 +68,64 @@ cat("\n xlab: ",options$xlab)
 cat("\n ylab: ",options$ylab)
 cat("\n output: ",options$output)
 
+#choosing whether to plot data as multiple groups on one plot(factoring) OR multiple groups on different plots
+if(options$factor == "Multiple"){
+     gg_facet = facet_wrap( ~ factor)
+     gg_factor = NULL
+     color_scale = NULL
+          
+          if(options$points == "Default"){
+            gg_point = geom_point(size=1, alpha=1, gg_factor)
+        } else {
+            gg_point = geom_point(size=options$size, alpha=options$alpha, colour=options$pointcolor)
+            cat("\n size: ",options$size)
+            cat("\n alpha: ",options$alpha)
+            cat("\n pointcolor: ",options$pointcolor)
+        }
+
+    names(input)[options$factorcol] <- "factor"
+   
+    cat("\n factor: ",options$factor)
+    cat("\n factorcol: ",options$factorcol)
+} else if(options$factor == "Single"){
+    gg_facet = NULL
+    gg_factor = aes(colour=factor(factor))
+
+          if(options$points == "Default"){
+            gg_point = geom_point(size=1, alpha=1, gg_factor)
+        } else {
+            gg_point = geom_point(size=options$size, alpha=options$alpha, gg_factor)
+            cat("\n size: ",options$size)
+            cat("\n alpha: ",options$alpha)
+            cat("\n pointcolor: ",options$pointcolor)
+        }
+
+      if(options$colors == "Default"){
+          color_scale = scale_colour_hue(direction=options$colororder)
+      } else {
+          color_scale = scale_color_brewer(palette=options$colors, direction=options$colororder)
+      }
+
+    names(input)[options$factorcol] <- "factor"
+    
+    cat("\n factor: ",options$factor)
+    cat("\n factorcol: ",options$factorcol)
+    cat("\n color_scale: ",options$colors)
+    cat("\n color_order: ",options$colororder)
+} else{
+      gg_facet = NULL
+      gg_factor = NULL
+      color_scale = NULL
+
+        if(options$points == "Default"){
+            gg_point = geom_point(size=1, alpha=1, gg_factor)
+        } else {
+            gg_point = geom_point(size=options$size, alpha=options$alpha, colour=options$pointcolor)
+            cat("\n size: ",options$size)
+            cat("\n alpha: ",options$alpha)
+            cat("\n pointcolor: ",options$pointcolor)
+        }    
+}
 
 if(options$scaling == "Automatic"){
     gg_scaley = NULL
@@ -78,7 +158,40 @@ if(options$drawquartiles == "none"){
     gg_quartile = c(0.25, 0.5, 0.75)
 }
 
+#axis label custization
+if(options$axistitlecust == "Default"){
+    gg_axistitle = theme(axis.title = element_text(color = NULL, size = NULL, face = NULL))
+} else {
+    gg_axistitle = theme(axis.title = element_text(color = options$axistitlecolor, size = options$axistitlesize, face = options$axistitleface))
+}
 
+
+#axis text(tick) custization
+if(options$axistextcust == "Default"){
+    gg_axistext = theme(axis.text = element_text(color = NULL, size = NULL, face = NULL))
+} else {
+    gg_axistext = theme(axis.text = element_text(color = options$axistextcolor, size = options$axistextsize, face = options$axistextface))
+}
+
+
+#plot title custimization
+if(options$plottitlecust == "Default"){
+    gg_plottitle = theme(plot.title = element_text(color = NULL, size = NULL, face = NULL))
+} else {
+    gg_plottitle = theme(plot.title = element_text(color = options$plottitlecolor, size = options$plottitlesize, face = options$plottitleface))
+}
+
+#grid line customization
+if(options$gridlinecust == "Default"){
+    gg_gridline = NULL
+} else if(options$gridlinecust == "hidemajor"){
+    gg_gridline = theme(panel.grid.major = element_blank())
+} else if(options$gridlinecust == "hideminor"){
+    gg_gridline = theme(panel.grid.minor = element_blank())
+} else if(options$gridlinecust == "hideboth"){
+    gg_gridline = theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank())
+} else {
+}
 
 ggplot(input,aes(variable,value)) +geom_violin(scale = "area", colour = options$ggcolor, fill = options$ggfill, draw_quantiles = gg_quartile) +
 gg_scaley+theme_bw()+xlab(options$xlab)+ylab(options$ylab)+ggtitle(options$title)
